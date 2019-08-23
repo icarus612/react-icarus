43fbc70cc8a2ace9bb5f499251248595892b63d4
icarus <ellishogan95@gmail.com>
2019-08-23 00:14:32 -0400

    all

--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -11,19 +11,17 @@ let NavBar = (props)=> {
               }
     }
     return (
-        <navbar>
-            <nav>
+        <nav>
             <div className="d-flex flex-row justify-content-end align-items-center">
-                    <div className="d-flex flex-row justify-content-around align-items-center col-8 col-md-6 col-lg-4">
-                        <a href="/">Home</a>
-                        <a href="/about">About Us</a>
-                        <a href="/portfolio">Portfolio</a>
-                        <a href="/contact-us">Contact Us</a>
-                        <a href="/get-quote">Get Quote</a>
-                    </div>
+                <div className="d-flex flex-row justify-content-around align-items-center col-8 col-md-6 col-lg-4">
+                    <a href="/">Home</a>
+                    <a href="/about">About Us</a>
+                    <a href="/portfolio">Portfolio</a>
+                    <a href="/contact-us">Contact Us</a>
+                    <a href="/get-quote">Get Quote</a>
                 </div>
-            </nav>
-        </navbar>
+            </div>
+        </nav>
        
     )
 }
